a84255a1ea7bbe43669e9487f1d05f1f26ad4f15
Andres Carvajal <jc1993box@hotmail.com>
2020-04-30 04:02:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,10 @@
 class Dog 
-    def name= (dog_name)
+  def name= (dog_name)
     @name= dog_name
   end 
-
   
-  
-  
-  # describe '#name=' do
-  #     it 'writes the name of the dog to an instance variable @name' do
-  #       fido = Dog.new
-  #       fido.name = "Fido"
+  def name
+    @name
+  end
 
-  # def initialize(dog_name)
-  #   @dog_name = name
-  # end 
-
-
-    
-  # def name
-  #   @new_name
-  # end
 end
